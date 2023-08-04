6c656fb1ddde63b679c9011033cf8aa707a99a17
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-08-04 10:43:01 +0100

    adding the user to get just les les word du user

--- app/Http/Controllers/VocabulaireController.php
+++ app/Http/Controllers/VocabulaireController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class VocabulaireController extends Controller
 {
     public function index()  {
-        $vocabulaires=Vocabulaire::query()->paginate(10);
+        $vocabulaires=Vocabulaire::query()->where('user',auth()->user()->id)->paginate(10);
         return view('index',compact('vocabulaires'));
     } 
     public function create()  {
@@ -21,22 +21,33 @@ class VocabulaireController extends Controller
             'definition'=>'required|string',
             'language'=>'required|in:1,2,3',
         ]);
-        $quiz_id= Quiz::query()->latest('id')->first()->id;
+        $quiz= Quiz::query()->latest('id')->first();
+        if($quiz){
+            $quiz_id = $quiz->id ;
+            $number_words=Vocabulaire::where('quiz',$quiz_id)->count();
 
-        $number_words=Vocabulaire::where('quiz',$quiz_id)->count();
-
-        // dd($number_words);
-        if($number_words>10){
-           $quiz_id= Quiz::create([
-                'title'=>'quiz number '.$quiz_id+2,
+            // dd($number_words);
+            if($number_words>10){
+                $quiz_id= Quiz::create([
+                    'title'=>'quiz number '.$quiz->id+2,
+                    'score_max'=>0,
+                    'last_score'=>0,
+                    'language'=>$validate['language'],
+                    ])->id;
+                    unset($validate['language']);
+                    
+                }
+        }else{
+            $quiz_id= Quiz::create([
+                'title'=>'quiz number 1 ',
                 'score_max'=>0,
                 'last_score'=>0,
                 'language'=>$validate['language'],
-            ])->id;
-            unset($validate['language']);
+                ])->id;
 
         }
         $validate['quiz']=$quiz_id;
+        $validate['user']=auth()->user()->id;
         // dd($quiz_id);
         $vocabulaire=Vocabulaire::create($validate);
         return redirect( route('vocabulaire.index'));
